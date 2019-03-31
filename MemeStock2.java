184469f06fa24f674aea7680fa8aaa163af74243
logbasetwo <mjb@balogh-family.com>
2019-03-31 10:43:26 -0400

    Add files via upload

---
+++ MemeStock2.java
@@ -0,0 +1,70 @@
+package economeme;
+
+import java.util.ArrayList;
+
+public class MemeStock{
+
+	private String meme;
+	private int[] values;
+	//private int shares;
+	
+	
+	
+	public MemeStock(String meme, int[] values)
+	{
+		this.meme = meme;
+		this.values = values;
+		//this.shares = shares;
+	}
+	
+	public String getMeme()
+	{
+		return meme;
+	}
+	
+	public int getValue(int date)
+	{
+		return values[date];
+	}
+	
+	public int[] getValues()
+	{
+		return values;
+	}
+	/**
+	public void setShares(int amount)
+	{
+		shares = amount;
+	}
+	
+	public int getShares()
+	{
+		return shares;
+	}
+	
+	public int valueChangePerShare(int dateBought, int currentDate)
+	{
+		return values[currentDate] - values[dateBought];
+	}
+	
+	**/
+	public boolean equals(Object other)
+	{
+		return meme.equals(((MemeStock) other).getMeme());
+	}
+	
+	
+	
+	public String toString()
+	{
+		String numString = "{";
+		for(int num: values)
+		{
+			numString = numString.concat(num + " ");
+		}
+		numString = numString.concat("}");
+		return "\tmeme: " + meme + "\n\tprice: " + numString;
+	}
+	
+	
+}
